336076bae88923cb4a2cbc07ca3bd662288123c7
franrios <fcojriosbello@gmail.com>
2017-12-26 12:19:33 +0100

    Map ready with some markers

--- src/App.js
+++ src/App.js
@@ -1,43 +1,84 @@
-import React, { Component } from 'react';
-import ReactMapGL, { Marker } from "react-map-gl"
+import React, { Component } from "react"
+import ReactMapGL, { Marker, NavigationControl } from "react-map-gl"
+import DeckGL, { LineLayer } from "deck.gl"
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
-const MapboxAccessToken = 'pk.eyJ1IjoiZnJhbnJpb3MiLCJhIjoiY2pibXM5Z2k5M3I4dTMzbHB4NWJkanNyNSJ9.24SqQxgDaPDZMS2YT51AFA'
+const locations = require("./location_history").locations
+const locationHistory = locations.filter(
+  (item, i) => i % Math.floor(locations.length / 300) === 0
+)
+
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoiZnJhbnJpb3MiLCJhIjoiY2pibXM5Z2k5M3I4dTMzbHB4NWJkanNyNSJ9.24SqQxgDaPDZMS2YT51AFA"
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
-    this.state = {
-      latitude: 50,
-      longitude: 50,
-      zoom: 9
-    }
+    this.state = { latitude: 40.416775, longitude: -3.70379, zoom: 3.5 } //Madrid location
   }
 
-  componentWillMount () {
-    navigator.geolocation.getCurrentPosition(({ coords }) => {
-      const { latitude, longitude } = coords
-      this.setState({ latitude, longitude })
-    })
+  onViewportChange(viewport) {
+    const { latitude, longitude, zoom } = viewport
+    this.setState({ latitude, longitude, zoom })
+    // Optionally call `setState` and use the state to update the map.
   }
+
   render() {
     const { innerWidth: width, innerHeight: height } = window
     const { latitude, longitude, zoom } = this.state
 
-    return <div>
-        <ReactMapGL width={width} height={height} latitude={latitude} longitude={longitude} zoom={zoom} onViewportChange={viewport => {
-            const { latitude, longitude, zoom } = viewport
-            this.setState({latitude, longitude, zoom})
-            // Optionally call `setState` and use the state to update the map.
-          }}>
-                  <Marker latitude={37.78} longitude={longitude + 0.001} latitude={latitude + 0.001} offsetLeft={-20} offsetTop={-10}>
-          <div>You are here</div>
-        </Marker>
-          </ReactMapGL>
+    const viewport = {
+      width,
+      height,
+      longitude,
+      latitude,
+      zoom,
+      pitch: 0,
+      bearing: 0
+    }
+
+    const data = [
+      {
+        sourcePosition: [latitude, longitude],
+        targetPosition: [latitude + 0.1, longitude + 0.1]
+      }
+    ]
+    console.log({ locationHistory })
+    return (
+      <div>
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          mapStyle="mapbox://styles/mapbox/basic-v9"
+          onViewportChange={(vp) => this.onViewportChange(vp)}
+        >
+          <div style={{ position: "absolute", right: 0, bottom: 0 }}>
+            <NavigationControl onViewportChange={(vp) => this.onViewportChange(vp)} />
+          </div>
+          {locationHistory.map(({ longitudeE7, latitudeE7, timestampMs }) => (
+            <Marker
+              key={timestampMs}
+              longitude={longitudeE7 / 1e7}
+              latitude={latitudeE7 / 1e7}
+            >
+              <div className="marker" />
+            </Marker>
+          ))}
+          <DeckGL
+            {...viewport}
+            layers={[
+              new LineLayer({
+                id: "line-layer",
+                data
+              })
+            ]}
+          />
+        </ReactMapGL>
       </div>
+    )
   }
 }
 
-export default App;
+export default App
